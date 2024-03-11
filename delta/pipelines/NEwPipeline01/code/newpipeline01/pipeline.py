8e61f134c7efe6d63f4afb3ac4058195cd7ef6a2
pankaj.bhardwaj.2100@gmail.com <pankaj.bhardwaj.2100@gmail.com>
2024-03-11 12:09:46 +0000

    🔁 Modified     - Pipeline        - NEwPipeline01

--- delta/pipelines/NEwPipeline01/code/newpipeline01/pipeline.py
+++ delta/pipelines/NEwPipeline01/code/newpipeline01/pipeline.py
@@ -1,7 +1,6 @@
 from pyspark.sql import *
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
-from prophecy.utils import *
 from newpipeline01.config.ConfigStore import *
 from newpipeline01.udfs.UDFs import *
 from prophecy.utils import *
